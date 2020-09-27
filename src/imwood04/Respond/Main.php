ae403bb0c0c84105fd9fb01fe7eb5e648ed06ccd
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-27 17:32:23 -0400

    No CLue anymore xD

--- src/imwood04/Respond/Main.php
+++ src/imwood04/Respond/Main.php
@@ -4,31 +4,29 @@ declare(strict_types=1);
 
 namespace Imwood04\Responder;
 
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\plugin\PluginBase;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase{
-    public function onLoad()  : void{
-        $this->getLogger()->info(TextFormat::WHITE . "I've been loaded!");
-    }
-    public function onEnable() : void{
-        $this->getServer()->getPluginManager()->registerEvents(new Listen($this), $this);
-        $this->getScheduler()->scheduleRepeatingTask(new Broadcast($this->getServer()), 120);
-        $this->getLogger()->info(TextFormat::DARK_GREEN . "Chat Responder Enabled!");
-    }
-    public function onDisable() : void{
-        $this->getLogger()->info(TextFormat::DARK_RED . "Chat Responder Disabled!");
-    }
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
-            case "Respond":
-                $sender->sendMessage("This Responded to your message " . $sender->getName() . "!");
+use pocketmine\Listener;
 
-                return true;
-            default:
-                return false;
+class main extends PluginBase implements Listener {
+    public function onEnable(){
+        $this->getLogger()->info("Respond Enabled!");
+
+    }
+    public function onDisable(){
+        $this->getLogger()->info("Respond Disabled!");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+        switch ($cmd->getName()){
+            case "respond";
+             $sender->sendMessage("test works!");
         }
+    return true;
     }
 }
