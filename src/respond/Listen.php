48bf50c4ccad553fd784b2b12295b1ac72e9b17a
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-27 15:49:42 -0400

    Added Stuffz

---
+++ src/respond/Listen.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExamplePlugin;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerRespawnEvent;
+
+class ExampleListener implements Listener{
+
+    /** @var Main */
+    private $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * @param PlayerRespawnEvent $event
+     *
+     * @priority        NORMAL
+     * @ignoreCancelled false
+     */
+
+    public function onSpawn(PlayerRespawnEvent $event) : void{
+        $this->plugin->getServer()->broadcastMessage($event->getPlayer()->getDisplayName() . " has just spawned!");
+    }
+}
