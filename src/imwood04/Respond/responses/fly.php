76b2ae89c13865332acfa8ebf2aa87caa6505f34
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-28 22:06:11 -0400

    ...

--- src/imwood04/Respond/responses/fly.php
+++ src/imwood04/Respond/responses/fly.php
@@ -5,8 +5,14 @@ namespace imwood04\Respond\responses;
 
 
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\CommandException;
 
-abstract class fly extends Command
+class fly extends Command
 {
 
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        // TODO: Implement execute() method.
+    }
 }
