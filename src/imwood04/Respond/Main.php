6f865e24185edde2c20d0ba2136a192576272475
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-28 21:55:37 -0400

    ...

--- src/imwood04/Respond/Main.php
+++ src/imwood04/Respond/Main.php
@@ -2,12 +2,13 @@
 declare(strict_types=1);
 namespace Imwood04\Responder;
 
-use pocketmine\Player;
+use imwood04\Respond\responses\fly;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase{
+class re extends PluginBase
+{
 
     private static $instance;
 
@@ -19,16 +20,18 @@ class Main extends PluginBase{
         return self::$instance;
     }
 
-    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
-        switch ($cmd->getName()){
-            case "respond";
-             $sender->sendMessage("test works!");
-             if($sender instanceof Player){
-                 $sender->sendMessage("Command Works");
-             } else {
-                 $sender->sendMessage("Command sent in Console");
-             }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args): bool{
+        $respond = $command->getName();
+        if ($respond === "respond") {
+            $sender->sendMessage("Hi!");
+            return true;
         }
-    return true;
+        return false;
+    }
+
+    public function onCommands()
+    {
+        $this->getServer()->getCommandMap()->register('fly', new fly($this));
+
     }
 }
