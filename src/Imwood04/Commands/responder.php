d09e12738012f84c3fb8b207d26e9634a81fdf64
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-29 00:57:54 -0400

    ..

---
+++ src/Imwood04/Commands/responder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Imwood04\Respond\Commands;
+
+use Imwood04\Respond\Main;
+use pocketmine\Player;
+use pocketmine\command;
+use pocketmine\command\CommandSender;
+
+class responder extends command\Command
+{
+    private $main;
+    public function __construct(Main $main)
+    {
+        parent::__construct("fly");
+        $this->setPermission("cloud.fly");
+        $this->setDescription("Fly Command");
+        $this->setPermissionMessage("§bResponder §4=> §f§c§cYou do not have Perms to use this command1");
+        $this->main = $main;
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if ($sender instanceof Player) {
+            if (!$this->testPermission($sender)) return;
+            if (empty($args[0])) {
+                $name = $sender->getName();
+                if (!in_array($name, $this->main->command)) {
+                    $this->main->command[] = $name;
+                    $sender->sendMessage($this->main->prefix . "§aFlight Activated");
+                    $sender->setAllowFlight(true);
+                } else {
+                    unset($this->main->command[array_search($name, $this->main->command)]);
+                    $player = $sender->getPlayer();
+                    $sender->sendMessage($this->main->prefix . "§cFlying Deactivated");
+                    $sender->setAllowFlight(false);
+                    $sender->setFlying(false);
+                }
+            }
+        }
+    }
+}
